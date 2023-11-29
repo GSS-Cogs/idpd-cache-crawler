ebff4ed37334d04f4ea2cdfa203eaddd22df8247
mike <mikelovesbooks@gmail.com>
2023-11-29 08:55:38 +0000

    more restructuing

--- src/crawlers/base.py
+++ src/crawlers/base.py
@@ -30,17 +30,6 @@ class BaseCrawler(mata=ABCMeta):
         """
 
         ...
-    
-    # TODO - not an abstract, implement this here.
-    # Note: the "something" below will be some variation of log
-    # out a useful message (basic logs to being with, structured
-    # logs can follow once the crawlers built).
-    # The main this is THE CRAWLER SHOULD NEVER EVER RAISE an 
-    # uncontrolled error, as it needs to keep going.
-    def handle_error(self, url: str, msg: str, error: Exception):
-        """
-        Do something where a crawl encounters an error.
-        """
 
     # NOTE - not an abstract, a get (with a no cache header) is
     # the same for all crawlers
@@ -49,12 +38,8 @@ class BaseCrawler(mata=ABCMeta):
     # with NO CACHE headers (use self.domain here).
     # TODO 2 - should have exponential backoff to retry in the
     # event of errors - see https://pypi.org/project/backoff/
-    # NOTE - you can let this raise an error but you will need
-    # to catch and control it in the calling code 
     # NOTE - do not put the delay between gets in the implemented crawl()
-    # function not here, we want to be able to finsesse the delay
-    # base on the crawler (some services will want a lighter touch
-    # than others).
+    # function not here.
     # Note: you dont need to do anything with the get or return
     # anything, just make sure its an acceptable (i.e it worked)
     # status code. If you don't get an acceptable code, log the
